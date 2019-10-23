bf4f72ce84e01b7cae257fd704979e8471fc31be
Laurel Mitton <lmitton@olin.edu>
2019-10-23 09:47:54 -0400

    first draft of pseudocode

---
+++ house_model.py
@@ -0,0 +1,28 @@
+# house class
+    # attributes:
+        # window number
+        # window location
+        # window insulation efficiency (percent heat retained)
+        # floor thermal mass
+        # rate of heat loss - how "leaky" etc.
+        # wall/floor insulation efficiency (percent heat retained)
+        # aux. heating on/off
+        # aux. heating efficiency
+        # volume of air in house
+    # functions:
+        # toggle aux heating
+        # timestep update
+# world class
+    # attributes:
+        # cloud cover (true/false)
+        # time of year
+        # sun position
+        # ambient temp.
+    # functions:
+        # update cloud cover
+        # update time of year
+        # update sun position
+        # change ambient temp.
+
+# global stuff
+    # time of day
