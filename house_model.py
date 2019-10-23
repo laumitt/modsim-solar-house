8b7fdcad67825843e2708dfa2a99e89b3986228b
Laurel Mitton <lmitton@olin.edu>
2019-10-23 10:44:16 -0400

    began sketching model - DOES NOT RUN

--- house_model.py
+++ house_model.py
@@ -1,10 +1,9 @@
 # house class
     # attributes:
-        # window number
-        # window location
+        # window sq. ft.
         # window insulation efficiency (percent heat retained)
         # floor thermal mass
-        # rate of heat loss - how "leaky" etc.
+        # rate of heat loss - how "leaky" etc. ACH range 0.35 to 0.5
         # wall/floor insulation efficiency (percent heat retained)
         # aux. heating on/off
         # aux. heating efficiency
@@ -12,6 +11,18 @@
     # functions:
         # toggle aux heating
         # timestep update
+class House:
+    def init(self):
+        self.window_percentage = 2.22 # window size as a percentage of sq. footage
+        self.window_insulation = 
+        self.thermal_mass      =
+        self.heat_loss         =
+        self.wall_insulation   =
+        self.area              = 2500 # square feet
+        self.volume            = 63000 # cubic feet
+        self.current_heat      =
+    def update(self, timestep):
+        self.current_heat = self.
 # world class
     # attributes:
         # cloud cover (true/false)
